d65fcf107571e3305354556ba13b2a6496faa1bc
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-05 15:14:48 -0300

    fix status bar

--- App.tsx
+++ App.tsx
@@ -4,11 +4,17 @@ import { ThemeProvider } from 'styled-components'
 import { Groups } from '@screens/Groups'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle={'light-content'} 
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
