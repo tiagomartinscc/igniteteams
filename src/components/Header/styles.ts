ea7bc4fbdb534c61b4af942dda94d0b70d9a7364
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-05 15:25:11 -0300

    create Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content:center;
+`;
+
+export const Logo = styled.Image`
+  width: 46px;
+  height: 55px;
+`
