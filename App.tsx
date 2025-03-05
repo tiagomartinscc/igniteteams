09ee86438d9b137c5c60a99e20f398af9c56b206
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-05 15:10:53 -0300

    create Loading component

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import theme from './src/theme/index'
 import { ThemeProvider } from 'styled-components'
 import { Groups } from '@screens/Groups'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
