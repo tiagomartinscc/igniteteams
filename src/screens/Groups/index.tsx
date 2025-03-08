7aa1c4be824b6617f9d1f593966725d77e68b9f9
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-08 16:23:40 -0300

    GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,17 @@
 import {Container} from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
-
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   return (
     <Container>
       <Header />
-      <Highlight title='Turmas' subtitle='Jogue com a sua turma'>
-        
-      </Highlight>
+      <Highlight 
+        title='Turmas' 
+        subtitle='Jogue com a sua turma' 
+      />
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
