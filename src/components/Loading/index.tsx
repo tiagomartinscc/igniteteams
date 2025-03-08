0bce24d4674a01fc570995980ebe3a06888ac7ab
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-08 16:02:47 -0300

    Highlight component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Container, LoadingIndicator } from './styles'
 
 export function Loading() {
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import {Container} from './styles'
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
+
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma'>
+        
+      </Highlight>
     </Container>
   );
 }
