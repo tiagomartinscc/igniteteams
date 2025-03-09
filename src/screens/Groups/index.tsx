38f24f84f68ef38291af64458b70463d8e15baea
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-08 21:30:54 -0300

    create flatlist into Group screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
+
 import {Container} from './styles'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Galera do Rocket'])
   return (
     <Container>
       <Header />
@@ -11,7 +16,16 @@ export function Groups() {
         title='Turmas' 
         subtitle='Jogue com a sua turma' 
       />
-      <GroupCard title='Galera do Ignite' />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
+      
     </Container>
   );
 }
