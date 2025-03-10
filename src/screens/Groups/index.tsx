03d0a6e1f7fac9db6823033a77de3d2b120cf47e
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-10 16:36:54 -0300

    typing routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import {Container} from './styles'
 
@@ -11,6 +12,12 @@ import { Button } from '@components/Button'
 
 export function Groups() {
   const [groups, setGroups] = useState([])
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
+
   return (
     <Container>
       <Header />
@@ -37,7 +44,8 @@ export function Groups() {
       />
 
       <Button 
-        title='Criar nova turma' 
+        title='Criar nova turma'
+        onPress={handleNewGroup}
       />
       
     </Container>
