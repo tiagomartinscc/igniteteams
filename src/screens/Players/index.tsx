24c35b1240f561ac60775f841d8b7c498cf34cb8
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-10 09:21:49 -0300

    Filter Component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
 import { ButtonIcon } from "@components/ButtonIcon"
 import { Input } from "@components/Input"
+import { Filter } from "@components/Filter"
 
 export function Players() {
   return (
@@ -23,6 +24,11 @@ export function Players() {
           icon="add"
         />
       </Form>
+
+      <Filter 
+        title="Time A"
+        isActive
+      />
     </Container>
   )
 }
